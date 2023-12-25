2e32425bc14c9e0544c23a14a74954b34baee4a7
DmytroDemyrskyi <long28469@gmail.com>
2023-12-25 11:24:34 +0200

    delete comments

--- school/middleware.py
+++ school/middleware.py
@@ -7,7 +7,6 @@ def log_middleware(get_response):
         response = get_response(request)
         execution_time = time.time() - start_time
 
-        # Зберігаємо параметри в текстовий файл
         with open("log.txt", "a") as log_file:
             log_file.write(
                 f"Path: {request.path}, Method: {request.method}, Execution time: {execution_time}\n"
